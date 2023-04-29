ac96e5c387e4e14f0a924e15d5dc91437bba4afc
PIARDSylvian <piard.sylvian@gmail.com>
2023-04-29 21:03:42 +0200

    footer zise & main nav shadow

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,15 +2,26 @@ import styles from '@/styles/Navbar.module.scss'
 import styled from 'styled-components'
 
 export const StyledNav = styled.nav`
-  ${(props) => (props.column ? `width: 117px;` : `padding-left: 25px;`)};
+  ${(props) =>
+    props.column
+      ? `width: 117px;`
+      : `padding-left: 25px; box-shadow: 5px 5px 5px 0px gray;`};
 
   ul {
     ${(props) =>
       props.column &&
       `
-      flex-direction: column;
-      align-items: center;
-      justify-content: space-evenly;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+      `};
+
+    ${(props) =>
+      !props.column &&
+      `
+        a {
+          padding: .5em;
+        }
       `};
   }
 `
