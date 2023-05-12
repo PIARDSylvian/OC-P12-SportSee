49ecc76dc6587aa62a9c39197a95c761ef7114c7
PIARDSylvian <piard.sylvian@gmail.com>
2023-05-12 16:33:36 +0200

    utils jsDoc

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,12 @@ import Performance from '@/entities/performance'
 
 export const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
+/**
+ * get User data by user Id
+ *
+ * @param {number} id
+ * @returns {User.data}
+ */
 export async function getUser(id) {
   return await fetch(`${process.env.BACKEND}/user/${id}`)
     .then(async (res) => {
@@ -17,6 +23,13 @@ export async function getUser(id) {
     })
 }
 
+/**
+ * get api route by type
+ *
+ * @param {number} id
+ * @param {string} type
+ * @returns {string}
+ */
 export function getApiRoute(id, type) {
   switch (type) {
     case 'activity':
@@ -30,6 +43,12 @@ export function getApiRoute(id, type) {
   }
 }
 
+/**
+ * get user activity by user
+ *
+ * @param {number} id
+ * @returns {Activity.data}
+ */
 export async function getActivity(id) {
   return await fetch(`${process.env.BACKEND}/user/${id}/activity`)
     .then((res) => res.json())
@@ -37,6 +56,12 @@ export async function getActivity(id) {
     .then((res) => new Activity(res).data())
 }
 
+/**
+ * get user average sessions by user
+ *
+ * @param {number} id
+ * @returns {AverageSessions.data}
+ */
 export async function getAverageSessions(id) {
   return await fetch(`${process.env.BACKEND}/user/${id}/average-sessions`)
     .then((res) => res.json())
@@ -44,6 +69,12 @@ export async function getAverageSessions(id) {
     .then((res) => new AverageSessions(res).data())
 }
 
+/**
+ * get user performance by user
+ *
+ * @param {number} id
+ * @returns {Performance.data}
+ */
 export async function getPerformance(id) {
   return await fetch(`${process.env.BACKEND}/user/${id}/performance`)
     .then((res) => res.json())
