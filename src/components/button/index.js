e440147aa5802080c00f65be6f5f0e6938d83a20
PIARDSylvian <piard.sylvian@gmail.com>
2023-05-12 12:16:27 +0200

    button jsDoc

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,12 @@
 import styles from '@/styles/Button.module.scss'
 import PropTypes from 'prop-types'
 
+/**
+ * Create button
+ *
+ * @param {{children :React.ReactElement}} children
+ * @returns {React.ReactElement}
+ */
 export default function Button({ children }) {
   return <button className={styles.button}>{children}</button>
 }
